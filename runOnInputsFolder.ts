7f55a974f1780908b1394efb2844a6c3be1e64f6
Breck Yunits <breck7@gmail.com>
2020-03-13 15:47:50 -1000

    quick commit

--- runOnInputsFolder.ts
+++ runOnInputsFolder.ts
@@ -6,15 +6,17 @@ const { Meshcapade } = require("./Meshcapade")
 const authorizationResponse = require("./authorizationResponse")
 const options = require("./options")
 
+const inputFolder = __dirname + "/inputs"
+
 const runAll = async () => {
   const username = "SRLbodycomplab"
 
   const results = fs
-    .readdirSync("inputs")
+    .readdirSync(inputFolder)
     .filter(filePath => filePath.endsWith(".obj"))
     .map(filePath => {
-      const inputFilePath = "inputs/" + filePath
-      const outPutFilePath = "outputs/" + filePath + ".output.obj"
+      const inputFilePath = `${inputFolder}/${filePath}`
+      const outPutFilePath = `outputs/${filePath}.output.obj`
       const session = new Meshcapade(username, authorizationResponse.token, inputFilePath)
       return session.checkExistingJobOrStartNewAlignment(inputFilePath, outPutFilePath, options)
     })
